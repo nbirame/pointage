016e0eaf802b240654d17439adbc28d9c0b490ae
Birame NDIAYE <biramendiaye71@gmail.com>
2024-12-05 12:32:25 +0000

    refactor report

--- wizard/rapport_wizard.py
+++ wizard/rapport_wizard.py
@@ -58,17 +58,25 @@ class RapportWizard(models.TransientModel):
             if equipe_mission:
                 number_day_of_mission = 0
                 for agent in equipe_mission:
-                    if (agent.mission_id.state == "en_cours" or agent.mission_id.state == "terminer") and ((agent.mission_id.date_depart >= self.date_in_get_rapport and agent.mission_id.date_retour <= self.date_end_get_rapport) or (agent.mission_id.date_depart <= self.date_in_get_rapport and agent.mission_id.date_retour <= self.date_end_get_rapport) or (agent.mission_id.date_depart <= self.date_in_get_rapport and agent.mission_id.date_retour >= self.date_end_get_rapport)) and (
-                            agent.employee_id.id == employee.id):
+                    if (agent.mission_id.state == "en_cours" or agent.mission_id.state == "terminer") and agent.mission_id.date_depart >= self.date_in_get_rapport and agent.mission_id.date_retour <= self.date_end_get_rapport:
                         number_day_of_mission += self.nombre_jours_sans_weekend(agent.mission_id.date_depart,
                                                                                 agent.mission_id.date_retour)
-                        print(f"Nombre de jours en mission {number_day_of_mission} du {agent.mission_id.date_depart} au {agent.mission_id.date_retour}")
-                        print(f"Nombre de jours en conge {absence_days_hollidays}")
-                        print(f"Nombre de jours en ferier {number_day_of_party}")
                         number_of_days_absence_legal = absence_days_hollidays + number_day_of_party + number_day_of_mission
                         self.total_number_of_working_hours = int((self.nombre_jours_sans_weekend(self.date_in_get_rapport,
                                                                                             self.date_end_get_rapport) - number_of_days_absence_legal) * heure_travail.worked_hours)
-                        print(f"Nombre de jours a travailler {self.total_number_of_working_hours}")
+                    elif (agent.mission_id.state == "en_cours" or agent.mission_id.state == "terminer") and agent.mission_id.date_depart >= self.date_in_get_rapport and agent.mission_id.date_retour >= self.date_end_get_rapport:
+                        number_day_of_mission += self.nombre_jours_sans_weekend(agent.mission_id.date_depart,
+                                                                                self.date_end_get_rapport)
+                        number_of_days_absence_legal = absence_days_hollidays + number_day_of_party + number_day_of_mission
+                        self.total_number_of_working_hours = int((self.nombre_jours_sans_weekend(self.date_in_get_rapport,
+                                                                                            self.date_end_get_rapport) - number_of_days_absence_legal) * heure_travail.worked_hours)
+                    else:
+                        number_day_of_mission += self.nombre_jours_sans_weekend(self.date_in_get_rapport,
+                                                                                self.date_end_get_rapport)
+                        number_of_days_absence_legal = absence_days_hollidays + number_day_of_party + number_day_of_mission
+                        self.total_number_of_working_hours = int(
+                            (self.nombre_jours_sans_weekend(self.date_in_get_rapport,
+                                                            agent.mission_id.date_retour) - number_of_days_absence_legal) * heure_travail.worked_hours)
             else:
                 number_of_days_absence_legal = absence_days_hollidays + number_day_of_party
                 self.total_number_of_working_hours = int((self.nombre_jours_sans_weekend(self.date_in_get_rapport,
