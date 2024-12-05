b457bd304f3c0f8f2c4aaef9c6447246ad649eef
Birame NDIAYE <biramendiaye71@gmail.com>
2024-12-05 12:53:39 +0000

    refactor report

--- wizard/rapport_wizard.py
+++ wizard/rapport_wizard.py
@@ -65,7 +65,7 @@ class RapportWizard(models.TransientModel):
                         self.total_number_of_working_hours = int((self.nombre_jours_sans_weekend(self.date_in_get_rapport,
                                                                                             self.date_end_get_rapport) - number_of_days_absence_legal) * heure_travail.worked_hours)
                     elif (agent.mission_id.state == "en_cours" or agent.mission_id.state == "terminer") and agent.mission_id.date_depart >= self.date_in_get_rapport and agent.mission_id.date_retour >= self.date_end_get_rapport:
-                        number_day_of_mission += self.nombre_jours_sans_weekend(agent.mission_id.date_depart,
+                        number_day_of_mission += self.nombre_jours_sans_weekend(agent.mission_id.date_retour,
                                                                                 self.date_end_get_rapport)
                         number_of_days_absence_legal = absence_days_hollidays + number_day_of_party + number_day_of_mission
                         self.total_number_of_working_hours = int((self.nombre_jours_sans_weekend(self.date_in_get_rapport,
