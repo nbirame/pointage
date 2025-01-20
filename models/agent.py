94eded3a9bd2f7b4b7be8ec9f5b3a455c38ff550
Birame NDIAYE <biramendiaye71@gmail.com>
2025-01-20 16:44:12 +0000

    optimisation

--- models/agent.py
+++ models/agent.py
@@ -647,7 +647,7 @@ class Agent(models.Model):
             ('check_out', '>=', self.last_week_start_date()),
         ])
         for attendance in attendances:
-            if attendance.check_in.time() >= time(10, 0):
+            if attendance.check_in.time() >= time(9, 0):
                 liste_retard.append([attendance.employee_id.id, attendance.employee_id.name, attendance.check_in.time()])
                 # print(liste_retard)
             else:
