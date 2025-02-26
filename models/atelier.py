9467e94625512c30c9ebe30e6f01f5c46c8e8a61
nbirame <biramendiaye71@gmail.com>
2025-02-26 08:59:34 +0000

    correction atelier

--- models/atelier.py
+++ models/atelier.py
@@ -14,33 +14,11 @@ class Atelier(models.Model):
     ], "Type", default="journee")
     date_start = fields.Datetime(string="Date de debut", required=True)
     date_end = fields.Datetime(string="Date de fin", required=True)
-    date_from = fields.Date(string="Date de debut", compute="_compute_date_from", required=True)
-    date_to = fields.Date(string="Date de fin", compute="_compute_date_to", required=True)
     name = fields.Char(
         string="Atelier",
         required=True
     )
     _order = 'id desc'
 
-    # @api.depends("date_start", "date_end")
-    # def _compute_name(self):
-    #     for record in self:
-    #         if record.date_start and record.date_end:
-    #             start_date_str = fields.Date.to_string(record.date_start)
-    #             end_date_str = fields.Date.to_string(record.date_end)
-    #             record.name = "ATELIER-%s-%s" % (start_date_str, end_date_str)
-    #         else:
-    #             # Sinon, on doit s'assurer que `record.name` reçoit une valeur vide ou par défaut
-    #             record.name = "ATELIER-SANS-DATE"
-
-    @api.depends("date_start")
-    def _compute_date_from(self):
-        for record in self:
-            record.date_from = record.date_start
-
-    @api.depends("date_end")
-    def _compute_date_to(self):
-        for record in self:
-            record.date_to = record.date_end
 
 
